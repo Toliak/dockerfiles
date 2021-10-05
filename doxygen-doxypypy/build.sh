2f75f7c646d7caebb61ee873ac636b701cc38cf5
Toliak <toliakpurple@gmail.com>
2021-10-05 17:12:27 +0300

    [doxygen-doxypypy] Version 1.0.4

--- doxygen-doxypypy/build.sh
+++ doxygen-doxypypy/build.sh
@@ -4,6 +4,6 @@ docker build -f Dockerfile . --tag toliak/doxygen-doxypypy:latest
 docker tag toliak/doxygen-doxypypy:latest toliak/doxygen-doxypypy:$(cat ./version)
 
 mkdir -p ~/docker
-docker save toliak/doxygen-doxypypy:latest | gzip > ~/docker/doxygen-doxypypy.tar.gz
+docker save toliak/doxygen-doxypypy:latest toliak/doxygen-doxypypy:$(cat ./version) | gzip > ~/docker/doxygen-doxypypy.tar.gz
 
 ls -la ~/docker
