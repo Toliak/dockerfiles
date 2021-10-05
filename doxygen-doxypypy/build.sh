c4fc54105005bd337bb893aff6ea944bbf88db66
Toliak <toliakpurple@gmail.com>
2021-10-05 15:19:17 +0300

    Doxygen doxypypy repo

---
+++ doxygen-doxypypy/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+docker build -f Dockerfile . --tag toliak/doxygen-doxypypy:latest
+docker tag toliak/doxygen-doxypypy:latest toliak/doxygen-doxypypy:$(cat ./version)
+
+mkdir -p ../docker
+docker save toliak/doxygen-doxypypy:latest | gzip > ../docker/doxygen-doxypypy.tar.gz
